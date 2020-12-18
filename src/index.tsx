a00cb0870f2d57e9528beb9fcf990bad7f549c01
Andres Sanabria <andrs.dev@gmail.com>
2020-12-18 15:44:12 -0600

    added redux-devtools-extension

--- src/index.tsx
+++ src/index.tsx
@@ -2,16 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import reducers  from 'stores';
 import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
+const composeEnhancers = composeWithDevTools({});
 const store = createStore(
   reducers, //Reducers,
-  {}, // Initial State,
-  applyMiddleware(thunk),
+  {}, // Initial State, 
+  composeEnhancers(
+    applyMiddleware(thunk)
+  ),
 )
 
 ReactDOM.render(
